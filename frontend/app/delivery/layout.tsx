0af3c8e40ff14addd31e613b3856a239ed6f3cff
bektemirovkam <55695037+bektemirovkam@users.noreply.github.com>
2023-12-28 13:13:14 +0600

    front

--- frontend/app/delivery/layout.tsx
+++ frontend/app/delivery/layout.tsx
@@ -10,7 +10,7 @@ export default function DeliveryLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+    <section className="flex-auto flex flex-col items-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg justify-center">{children}</div>
     </section>
   );
