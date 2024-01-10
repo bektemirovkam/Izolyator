ea61e2cda4f5ad1b1788f4979f54deb6f0579a61
kamil_bektemirov <kamil.bektemirov@finnplay.com>
2024-01-10 11:13:58 +0600

    search

--- frontend/services/products.ts
+++ frontend/services/products.ts
@@ -25,8 +25,19 @@ export const getProductInfo = cache(async (slug: string) => {
 });
 
 export const searchProducts = async (search: string) => {
+
+  const searchParts = search.split(" ")
+
+  const searchParams: string[] = []
+
+  searchParts.forEach((searchPart, index) => {
+    searchParams.push(`filters[$or][${index * 2 + 1}][vendorCode][$contains]=${searchPart}&filters[$or][${index * 2 + 2}][name][$contains]=${searchPart}`
+    )
+  })
+
+
   const { data } = await axios.get<{ data: Product[]; meta: MetaStrapiInfo }>(
-    `products?filters[$or][0][vendorCode][$contains]=${search}&filters[$or][1][name][$contains]=${search}&filters[$or][2][description][$contains]=${search}&populate=category&populate=preview&pagination[pageSize]=1000`
+    `products?filters[$or][0][description][$contains]=${search}&${searchParams.join("&")}&populate=category&populate=preview&pagination[pageSize]=10000`
   );
   return data;
 };
