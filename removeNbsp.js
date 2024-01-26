afecc79c50f3a4eff0d3649adf67fcc355890054
bektemirovkam <55695037+bektemirovkam@users.noreply.github.com>
2024-01-26 19:39:20 +0600

    remove nbsp

--- removeNbsp.js
+++ removeNbsp.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const API_URL = "https://prompostavki.kz/cms"
-const ADMIN_API_TOKEN = "2b48f4e1e3445b80e22c2e5ae5023a7a5c3b0cebc5f2a27b4c9181ab0e47732456ac307983948848cf92183fec0829ae332844de9d888f120d6aadd9a1360ba411a6d6bddd71ac4997fa1352d485ee62b402673a4f6ccd95b0a08ec062572ed778704bc9c1d2a671102635929cf830e41bf5aaedf278ff623155ef61a75b0922"
+const ADMIN_API_TOKEN = "8008fcf50bed1f809af8e98574684d3872e4d8c65edb3f5b878d3a4395d792feb1dc74326f38d35fd6937dfc98a392772c2d594584cb1cc34b7d706a8248d6b984662e74f50b77051d7b160e4d30ac52ddc95e976a2cbdf1be52d9c78eab2039c7379cf622b16baeadbe1741b3be884b8d37bbaa7e4f4fd8e33f56616dc5ae42"
 
 const adminApi = axios.create({
     baseURL: `${API_URL}/api/`,
